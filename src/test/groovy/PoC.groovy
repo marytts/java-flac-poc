5b9cdd5439587e110e3b5e3a1f427ba391d879c8
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-06-07 14:29:43 +0200

    add and copy some test resources, read them in test

--- src/test/groovy/PoC.groovy
+++ src/test/groovy/PoC.groovy
@@ -1,8 +1,20 @@
 import org.testng.annotations.*
+import marytts.util.data.audio.MaryAudioUtils
+import javax.sound.sampled.AudioSystem
 
 class PoC {
+
+    def tempDir
+
+    @BeforeClass
+    void setup() {
+        tempDir = new File(System.getProperty('tempDir'))
+    }
+
     @Test
     void test() {
-        assert true
+        def expectedFile = new File("$tempDir/expected.wav")
+        def expectedAIS = AudioSystem.getAudioInputStream(expectedFile)
+        MaryAudioUtils.getSamplesAsDoubleArray(expectedAIS)
     }
 }
