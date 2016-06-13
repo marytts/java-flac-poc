51c532cdb612f159a91e6cc89a799830625aa101
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-06-13 16:06:43 +0200

    logging

--- src/main/groovy/PoC.groovy
+++ src/main/groovy/PoC.groovy
@@ -1,7 +1,14 @@
+import groovy.util.logging.Log
+
+@Log
 class PoC {
 
-    def samples
+    PoC(File file) {
+        log.warning "$this should be loading $file"
+    }
 
-    PoC(File file) {}
+    double[] getSamples() {
+        log.warning "$this should be decoding the samples!"
+    }
 
 }
