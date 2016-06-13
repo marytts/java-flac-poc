fe3c7fce7633d627d94c27b88c6fb14c75954b0f
Sotaya <sotayamy@gmail.com>
2016-06-13 19:22:19 +0200

    Unstagged

--- src/main/groovy/PoC.groovy
+++ src/main/groovy/PoC.groovy
@@ -2,13 +2,16 @@ import groovy.util.logging.Log
 
 @Log
 class PoC {
+    def samples
 
     PoC(File file) {
         log.warning "$this should be loading $file"
+        this.samples = file
     }
 
     double[] getSamples() {
         log.warning "$this should be decoding the samples!"
+
     }
 
 }
